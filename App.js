2bcd08538b57bdb43f230d9cf69b02355637782d
Seph Soliman <sesoliman@tesla.com>
2022-08-09 16:14:23 -0700

    Added Samsung Galaxy S21 comparison, 10x automated runs

--- App.js
+++ App.js
@@ -8,37 +8,67 @@ import RnBlobUtil from 'react-native-blob-util';
 
 const dummyData = 'x'.repeat(5000000);
 
-const outputPath = `${Efs.cacheDirectory}efs-1mb.txt`;
+const outputPath = `${Efs.cacheDirectory}dummy-data.txt`;
 console.log('outputPath', outputPath);
 
+function getMedian(values = []) {
+  const medianValue = values.length % 2 === 1 ?
+    values[Math.ceil(values.length / 2)] :
+    (values[Math.floor(values.length / 2)] + values[Math.ceil(values.length / 2)]) / 2;
+  return medianValue;
+}
+
+function getAverage(values = []) {
+  return values.reduce((prev, cur) => prev + cur, 0) / values.length;
+}
+
+
 async function writeEfs() {
-  const start = Date.now();
-  await Efs.writeAsStringAsync(
-    outputPath,
-    dummyData,
-    { encoding: 'utf8' }
-  );
-  console.log('writeEfs, ms: ', Date.now() - start);
+  const measurements = [];
+  for (let i = 0; i < 10; i++) {
+    const start = Date.now();
+    await Efs.writeAsStringAsync(
+      outputPath,
+      dummyData,
+      { encoding: 'utf8' }
+    );
+    measurements.push(Date.now() - start);
+    console.log('writeEfs, ms: ', measurements.slice(-1)[0]);
+  }
+  console.log('writeEfs median', getMedian(measurements));
+  console.log('writeEfs avg', getAverage(measurements));
 }
 
 async function writeRnfs() {
-  const start = Date.now();
-  await Rnfs.writeFile(
-    outputPath,
-    dummyData,
-    { encoding: 'utf8' }
-  );
-  console.log('writeRnfs, ms: ', Date.now() - start);
+  const measurements = [];
+  for (let i = 0; i < 10; i++) {
+    const start = Date.now();
+    await Rnfs.writeFile(
+      outputPath,
+      dummyData,
+      { encoding: 'utf8' }
+    );
+    measurements.push(Date.now() - start);
+    console.log('writeRnfs, ms: ', measurements.slice(-1)[0]);
+  }
+  console.log('writeRnfs median', getMedian(measurements));
+  console.log('writeRnfs avg', getAverage(measurements));
 }
 
 async function writeRnBlobUtil() {
-  const start = Date.now();
-  await RnBlobUtil.fs.writeFile(
-    outputPath.replace('file://', ''),
-    dummyData,
-    'utf8',
-  );
-  console.log('writeRnBlobUtil, ms: ', Date.now() - start);
+  const measurements = [];
+  for (let i = 0; i < 10; i++) {
+    const start = Date.now();
+    await RnBlobUtil.fs.writeFile(
+      outputPath.replace('file://', ''),
+      dummyData,
+      'utf8',
+    );
+    measurements.push(Date.now() - start);
+    console.log('writeRnBlobUtil, ms: ', measurements.slice(-1)[0]);
+  }
+  console.log('writeRnBlobUtil median', getMedian(measurements));
+  console.log('writeRnBlobUtil avg', getAverage(measurements));
 }
 
 export default function App() {
--- README.md
+++ README.md
@@ -1,22 +1,26 @@
 # Test Setup
-iOS Simulator on an M1 Macbook Pro, running macOS 12.4 (21F79).
-A 5 MB file is written to disk on every button press.
-10 measurements of each library's writeFile()-equivalent function.
-1 warm-up write every time the library was switched in case there was any kind of caching going on.
+A 5 MB file is written 10 times to disk on every button press.
 
 # Results
 Benchmark of various React Native filesystem libraries:
 
+## iOS Simulator on an M1 Macbook Pro, running macOS 12.4 (21F79)
 ```
 | library                | milliseconds, median (aka p50) |
 |------------------------+--------------------------------|
 | expo-file-system       |                           43.5 |
-| react-native-fs        |                            217 |
+| react-native-fs        |                          217.0 |
 | react-native-blob-util |                             43 |
 ```
 
-## Other remarks
-Android emulator was casually tested as well, where RNFS had 3x worse performance (around 600ms per write), while the other libraries were around the same values as iOS. This would make sense since the emulator generally has a high CPU emulation overhead.
+## Samsung Galaxy S21 (SG-G991U), Android 12
+```
+| library                | milliseconds, median (aka p50) |
+|------------------------+--------------------------------|
+| expo-file-system       |                           47.0 |
+| react-native-fs        |                          937.0 |
+| react-native-blob-util |                           34.0 |
+```
 
 ## Conclusion
 `react-native-fs` is a mature filesystem library that is well maintained, however their choice to encode everything as base64 in transit between native and JS means it comes with a huge performance hit that (to my knowledge) cannot easily be worked around. The API itself will always encode as base64 despite choosing utf-8.
